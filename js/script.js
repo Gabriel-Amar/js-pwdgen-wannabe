0497b7aa8d4d7d6d0d83228095fe37a88440696d
Gabriel-Amar <amariutei.gabriel1@gmail.com>
2022-03-23 16:56:56 +0100

    js: flowchart

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+/*
+A[Creare una variabile] --> B[Dare un nome alla variabile]
+B --> C[Dare un valore alla variabile]
+C --> R[Dare come valore prompt con una domanda]
+R --> D[Fare un test con console.log] 
+D --> E{Funziona tutto?}
+E --> |Sì| F[Vado avanti]
+E --> |No| G[Esamino l'errore nella console]
+G --> I[Risolvo l'errore]
+I --> F
+F --> H[Copio il codice per 4 volte]
+H --> J[Cambio ad ogni sezione il nome della variabile e la domanda]
+J --> K[Prendo l'elemento document]
+K --> L[Uso il metodo getElementById]
+L --> M[Gli assegno un id precedentemente creato]
+M --> N[Uso innerHTML per prendere il contenuto dell'elemento con id]
+N --> O[Aggiunto un valore a questa variabile tramite +=]
+O --> P[Aggiungo tutti i nomi delle variabili precedenti precedute da un +]
+*/
+
 let userName = prompt("Inserisci il tuo nome");
 console.log(userName)
 
